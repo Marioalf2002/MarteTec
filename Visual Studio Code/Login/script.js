adc7fec84ee72a91f8b3dd692d2e8970a585b9b4
Mario Hernandez <marioalf200216@gmail.com>
2023-10-04 15:50:11 -0500

    Primeras Pruebas de implementacion de Login, carpeta separada de Visual Studio Code con Login, Carpeta con Logo

--- Visual Studio Code/Login/script.js
+++ Visual Studio Code/Login/script.js
@@ -1,11 +1,11 @@
-﻿const logregBox = document.querySelector('.logreg-box');
+const logregBox = document.querySelector('.logreg-box');
 const loginLink = document.querySelector('.login-link');
 const registerLink = document.querySelector('.register-link');
 
-registerLink.addEventListener('click', () => {
+registerLink.addEventListener('click', () =>{
     logregBox.classList.add('active');
 });
 
-loginLink.addEventListener('click', () => {
+loginLink.addEventListener('click', () =>{
     logregBox.classList.remove('active');
 });
