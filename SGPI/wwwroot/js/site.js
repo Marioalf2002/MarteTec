95077b40795c27bc3226bfaf9d074e1464162434
Mario Hernandez <marioalf200216@gmail.com>
2023-10-13 17:26:24 -0500

    Se Agrega vista de Admistrador, Errores por solucionar en DataTable

--- SGPI/wwwroot/js/site.js
+++ SGPI/wwwroot/js/site.js
@@ -1,32 +1,32 @@
-﻿//INICIO DE SESION
-const logregBox = document.querySelector('.logreg-box');
-const loginLink = document.querySelector('.login-link');
-const registerLink = document.querySelector('.register-link');
-
-registerLink.addEventListener('click', () => {
-    logregBox.classList.add('active');
-});
-
-loginLink.addEventListener('click', () => {
-    logregBox.classList.remove('active');
-});
-
-//MENU LATERAL
+﻿// MENU LATERAL
 $(".menu > ul > li").click(function (e) {
-    //Remove Active From Already Active
-    $(this).siblings().removeClass("active");
-    //Add Active To Clicked
-    $(this).toggleClass("active");
+    //Remove activar From Already activar
+    $(this).siblings().removeClass("activar");
+    //Add activar To Clicked
+    $(this).toggleClass("activar");
     //If Has Sub Menu Open It
     $(this).find("ul").slideToggle();
     //Close Other Sub Menu If Anu Open
     $(this).siblings().find("ul").slideUp();
-    //Remove Active Class Of Sub Menu Items
-    $(this).siblings().find("ul").find("li").removeClass("active");
+    //Remove activar Class Of Sub Menu Items
+    $(this).siblings().find("ul").find("li").removeClass("activar");
 });
 
-$(".menu-btn").click(function () {
-    $(".sidebar").toggleClass("active");
-    $(".menu-btn").toggleClass("active");
-    $(".contenedor").toggleClass("active");
+
+$(".menu-boton").click(function () {
+    $(".menu_lateral").toggleClass("activar");
+    $(".menu-boton").toggleClass("activar");
 })
+
+//INICIO DE SESION
+const logregBox = document.querySelector('.login-box');
+const loginLink = document.querySelector('.login-link');
+const registerLink = document.querySelector('.register-link');
+
+registerLink.addEventListener('click', () => {
+    logregBox.classList.add('activar');
+});
+
+loginLink.addEventListener('click', () => {
+    logregBox.classList.remove('activar');
+});
