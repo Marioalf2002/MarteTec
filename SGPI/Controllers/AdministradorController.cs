4590c6900b570f10c796424257686f03f8c16dd5
Mario Hernandez <marioalf200216@gmail.com>
2023-10-20 15:16:39 -0500

    tabla con datos de usuario

--- SGPI/Controllers/AdministradorController.cs
+++ SGPI/Controllers/AdministradorController.cs
@@ -3,17 +3,22 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 using SGPI.Models;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace SGPI.Controllers
 {
     public class AdministradorController : Controller
     {
         private readonly SgpiContext _context;
+        private readonly string CadenaSQL;
         SgpiContext dbcontext = new SgpiContext();
 
-        public AdministradorController(SgpiContext context)
+        public AdministradorController(SgpiContext context, IConfiguration config)
         {  
             _context = context;
+
+            CadenaSQL = config.GetConnectionString("cadenaSQL");
         }
 
         public IActionResult Admin()
@@ -24,6 +29,61 @@ namespace SGPI.Controllers
             ViewBag.Prog = dbcontext.Programas.ToList();
 
             return View();
+        }
+
+        [HttpGet]
+        public JsonResult ListaUsuarios()
+        {
+            List<Usuario> lista = new List<Usuario>();
+
+            using (var conexion = new SqlConnection(CadenaSQL))
+            {
+                conexion.Open();
+                var cmd = new SqlCommand("sp_ObtenerUsuarios", conexion);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                using (var dr = cmd.ExecuteReader())
+                {
+                    while (dr.Read())
+                    {
+                        lista.Add(new Usuario
+                        {
+                            Iduser = Convert.ToInt32(dr["Iduser"]),
+                            PrimerNombre = dr["primer_nombre"].ToString(),
+                            SegundoNombre = dr["segundo_nombre"].ToString(),
+                            PrimerApellido = dr["primer_apellido"].ToString(),
+                            SegundoApellido = dr["segundo_apellido"].ToString(),
+                            NumDoc = Convert.ToInt32(dr["num_doc"]),
+                            Email = dr["email"].ToString(),
+                            Pass = dr["pass"].ToString(),
+                            Documento = Convert.ToInt32(dr["documento"]),
+                            Genero = Convert.ToInt32(dr["genero"]),
+                            Programa = Convert.ToInt32(dr["programa"]),
+                            Rol = Convert.ToInt32(dr["rol"]),
+                            Activo = Convert.ToBoolean(dr["activo"])
+                        });
+
+                        //lista.Add(new Usuario
+                        //{
+                        //    Iduser = Convert.ToInt32(dr["Iduser"]),
+                        //    PrimerNombre = dr["PrimerNombre"].ToString(),
+                        //    SegundoNombre = dr["SegundoNombre"].ToString(),
+                        //    PrimerApellido = dr["PrimerApellido"].ToString(),
+                        //    SegundoApellido = dr["SegundoApellido"].ToString(),
+                        //    NumDoc = Convert.ToInt32(dr["NumDoc"]),
+                        //    Email = dr["Email"].ToString(),
+                        //    Pass = dr["Pass"].ToString(),
+                        //    Documento = Convert.ToInt32(dr["Documento"]),
+                        //    Genero = Convert.ToInt32(dr["Genero"]),
+                        //    Programa = Convert.ToInt32(dr["Programa"]),
+                        //    Rol = Convert.ToInt32(dr["Rol"]),
+                        //    Activo = Convert.ToBoolean(dr["Activo"])
+                        //});
+                    }
+                }
+            }
+
+            return Json(new { data = lista });
         }
     }
 }
--- SGPI/wwwroot/js/database.js
+++ SGPI/wwwroot/js/database.js
@@ -3,11 +3,13 @@ let dataTable;
 let dataTableIsInitialized = false;
 
 const dataTableOptions = {
-    //scrollX: "2000px",
+    /*dom: "<'controles-busqueda'fr>t<'row'<'col-sm-12 col-md-12 col-lg-12 col-xl-12'p>>", *//*// Esta es la línea que se agrega*/
+
+    scrollX: "2000px",
     lengthMenu: [5, 10, 15, 20],
     columnDefs: [
-        { classnombre: "centered", targets: [0, 1, 2, 3, 4, 5, 6] },
-        { orderable: false, targets: [5, 6] },
+        { classnombre: "centered", targets: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13] },
+        { orderable: false, targets: [12, 13] },
         /*{ searchable: false, targets: [1] }*/
         //{ width: "50%", targets: [0] }
     ],
@@ -44,19 +46,29 @@ const initDataTable = async () => {
 
 const listUsers = async () => {
     try {
-        const response = await fetch("https://jsonplaceholder.typicode.com/users");
-        const users = await response.json();
+        const response = await fetch("https://localhost:7020/Administrador/ListaUsuarios");
+        const result = await response.json();
+
+        // Accede a la propiedad "data" para obtener el arreglo de usuarios
+        const users = result.data;
 
         let content = '';
         users.forEach((user, index) => {
             content += `
                 <tr>
-                    <td>${index + 1}</td>
-                    <td>${user.name}</td>
+                    <td>${user.iduser}</td>
+                    <td>${user.primerNombre}</td>
+                    <td>${user.segundoNombre}</td>
+                    <td>${user.primerApellido}</td>
+                    <td>${user.segundoApellido}</td>
+                    <td>${user.documento}</td>
+                    <td>${user.numDoc}</td>
+                    <td>${user.genero}</td>
+                    <td>${user.programa}</td>
+                    <td>${user.rol}</td>
                     <td>${user.email}</td>
-                    <td>${user.address.city}</td>
-                    <td>${user.company.name}</td>
-                    <td><i class="bx bx-check" style="color: green;"></i></td>
+                    <td>${user.pass}</td>
+                    <td>${user.activo ? '<i class="bx bx-check" style="color: green;"></i>' : '<i class="bx bx-x" style="color: red;"></i>'}</td>
                     <td>
                         <button class="btn btn-sm btn-primary"><i class="bx bxs-pencil"></i></button>
                         <button class="btn btn-sm btn-danger"><i class="bx bxs-trash"></i></button>
